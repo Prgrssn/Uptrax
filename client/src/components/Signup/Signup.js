b5929e78f0a99f078b3073877540554023148170
Joshua Young <prgrssn@gmail.com>
2021-11-29 12:30:33 -0800

    add signup form fields and classes. get firebase auth to work.

--- client/src/components/Signup/Signup.js
+++ client/src/components/Signup/Signup.js
@@ -1,11 +1,16 @@
 import React, { useRef } from "react";
 import { Card, Button, Form } from "react-bootstrap";
 import { useAuth } from "../../contexts/AuthContext";
+import "./Signup.scss";
 
 export default function Signup() {
+  const firstNameRef = useRef();
+  const lastNameRef = useRef();
   const emailRef = useRef();
   const passwordRef = useRef();
   const passwordConfirmRef = useRef();
+  const astRef = useRef();
+  const firstAidRef = useRef();
   const { signup } = useAuth();
 
   const handleSubmit = (event) => {
@@ -15,28 +20,75 @@ export default function Signup() {
   };
 
   return (
-    <>
-      <Card>
-        <Card.Body>
-          <h2>Sign Up</h2>
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id="email">
-              <Form.Label>Email</Form.Label>
-              <Form.Control type="email" ref={emailRef} required />
+    <article className="login">
+      <Card className="login-card">
+        <Card.Body className="login-card__body">
+          <h2 className="login-card__header">Sign Up</h2>
+          <Form className="login-form">
+            <Form.Group id="firstname" className="login-form__group">
+              <Form.Label className="login-form__label">First Name</Form.Label>
+              <Form.Control
+                type="text"
+                ref={firstNameRef}
+                className="login-form__value"
+                required
+              />
             </Form.Group>
-            <Form.Group id="password">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" ref={passwordRef} required />
+            <Form.Group id="lastname" className="login-form__group">
+              <Form.Label className="login-form__label">Last Name</Form.Label>
+              <Form.Control
+                type="text"
+                ref={lastNameRef}
+                className="login-form__value"
+                required
+              />
             </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Confirm Password</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
+            <Form.Group id="email" className="login-form__group">
+              <Form.Label className="login-form__label">Email</Form.Label>
+              <Form.Control
+                type="email"
+                ref={emailRef}
+                className="login-form__value"
+                required
+              />
             </Form.Group>
+            <Form.Group id="password" className="login-form__group">
+              <Form.Label className="login-form__label">Password</Form.Label>
+              <Form.Control
+                type="password"
+                ref={passwordRef}
+                className="login-form__value"
+                required
+              />
+            </Form.Group>
+            <Form.Group id="password-confirm" className="login-form__group">
+              <Form.Label className="login-form__label">
+                Confirm Password
+              </Form.Label>
+              <Form.Control
+                type="password"
+                ref={passwordConfirmRef}
+                className="login-form__value"
+                required
+              />
+            </Form.Group>
+            <Form.Check
+              type="checkbox"
+              id="ast-checkbox"
+              label="I have my AST Certification"
+              ref={astRef}
+            />
+            <Form.Check
+              type="checkbox"
+              id="firstaid-checkbox"
+              label="I have First Aid Certification"
+              ref={firstAidRef}
+            />
             <Button type="submit">Sign Up</Button>
           </Form>
         </Card.Body>
       </Card>
       <div>Already Have an Account? Log in.</div>
-    </>
+    </article>
   );
 }
