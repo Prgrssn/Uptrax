147429a4311c9a4c7adf2627b4aa7d6037760f10
Joshua Young <prgrssn@gmail.com>
2021-12-01 12:24:56 -0800

    complete functionality of form fields and add validations

--- client/src/components/CreateProfile/CreateProfile.js
+++ client/src/components/CreateProfile/CreateProfile.js
@@ -8,6 +8,7 @@ export default function CreateProfile() {
   const displayNameRef = useRef();
   const astRef = useRef();
   const firstAidRef = useRef();
+  const expRef = useRef();
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   let navigate = useNavigate();
@@ -17,6 +18,31 @@ export default function CreateProfile() {
 
   const handleCreate = (event) => {
     event.preventDefault();
+
+    const displayName = displayNameRef.current.value;
+    const ast = astRef.current.value;
+    const firstAid = firstAidRef.current.value;
+    const yearsExp = expRef.current.value;
+    const firebaseID = currentUser.uid;
+
+    if (ast === "AST Certification") {
+      return setError("Please tell us if you have your AST");
+    }
+
+    if (firstAid === "First Aid Certification") {
+      return setError("Please tell us if you have First Aid certification");
+    }
+
+    if (yearsExp === "Years of Experience") {
+      return setError(
+        "Please let us know how many years of backcountry experience you have"
+      );
+    }
+
+    setError("");
+
+    const user = { displayName, ast, firstAid, yearsExp, firebaseID };
+    console.log(user);
   };
 
   const handleCancel = (event) => {
@@ -27,31 +53,38 @@ export default function CreateProfile() {
   return (
     <article className="create">
       <Card className="create-card">
-        <Card.Body className="lo-card__body">
-          <h2 className="login-card__header">Create Profile</h2>
+        <Card.Body className="create-card__body">
+          <h2 className="create-card__header">Create Profile</h2>
           {error && <Alert variant="danger">{error}</Alert>}
-          <Form className="login-form" onSubmit={handleCreate}>
-            <Form.Group id="firstname" className="login-form__group">
-              <Form.Label className="login-form__label">
+          <Form className="create-form" onSubmit={handleCreate}>
+            <Form.Group id="firstname" className="create-form__group">
+              <Form.Label className="create-form__label">
                 Display Name
               </Form.Label>
               <Form.Control
                 type="text"
+                placeholder="Display Name"
                 ref={displayNameRef}
-                className="login-form__value"
-                require
+                className="create-form__value"
+                required
               />
             </Form.Group>
             <Form.Group>
-              <Form.Select ref={astRef}>
+              <Form.Select ref={astRef} required>
                 <option>AST Certification</option>
-                <option value="yes">Yes</option>
-                <option value="no">No</option>
+                <option value="Yes">Yes</option>
+                <option value="No">No</option>
               </Form.Select>
-              <Form.Select ref={firstAidRef}>
+              <Form.Select ref={firstAidRef} required>
                 <option>First Aid Certification</option>
-                <option value="yes">Yes</option>
-                <option value="no">No</option>
+                <option value="Yes">Yes</option>
+                <option value="No">No</option>
+              </Form.Select>
+              <Form.Select ref={expRef} required>
+                <option>Years of Experience</option>
+                <option value="0-1">0-1 Years</option>
+                <option value="2-4">2-4 Years</option>
+                <option value="5+">5+ Years</option>
               </Form.Select>
             </Form.Group>
             <div className="login-form__buttons">
