ad6e2e2351319e374475cb6f7190773f2fad9bee
Joshua Young <prgrssn@gmail.com>
2021-12-03 14:55:15 -0800

    create new migration file and seed file

---
+++ server/migrations/20211203223806_create_user_tables.js
@@ -0,0 +1,16 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("users", function (table) {
+    table.increments("id");
+    table.string("firebase_id");
+    table.string("name").notNullable();
+    table.string("user_avatar");
+    table.integer("exp");
+    table.boolean("ast");
+    table.string("bio");
+    table.string("email");
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
+};
--- server/seeds/users_seed.js
+++ server/seeds/users_seed.js
@@ -5,23 +5,25 @@ exports.seed = function (knex) {
       return knex("users").insert([
         {
           id: 1,
-          firebase_id: "amNaoVRRLUUxMSYQ8NOyLdW38Yk1",
+          firebase_id: "tTNAXg8Rxrgf8ec2E8bsijiP7XD2",
           name: "Bill Nye",
           user_avatar:
             "https://cdn.geekwire.com/wp-content/uploads/2019/08/A9_06020-630x420.jpg",
           exp: 5,
           ast: true,
           bio: "Hey Kids lets learn about science and stuff!",
+          email: "billnye@science.com",
         },
         {
           id: 2,
-          firebase_id: "NDBqYcEKURSPWJ9I7hTdD9smQtm2",
+          firebase_id: "Gv11F4AI1rcFCthxTtOK04ivtAn2",
           name: "Robert Downey",
           user_avatar:
             "https://www.californiamuseum.org/sites/main/files/imagecache/medium/main-images/robertdowneyjr_cahalloffameinductee.png?1632975955",
           exp: 0,
           ast: false,
           bio: "I am IronMan",
+          email: "robert@downeyjr.com",
         },
         {
           id: 3,
@@ -32,6 +34,7 @@ exports.seed = function (knex) {
           exp: 5,
           ast: true,
           bio: "Let's code in the backcountry homie",
+          email: "ernie@ernie.com",
         },
       ]);
     });
