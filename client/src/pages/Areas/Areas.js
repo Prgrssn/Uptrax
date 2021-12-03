df12e788dc2756dcb743c4d0d18be42fa2566464
Joshua Young <prgrssn@gmail.com>
2021-12-02 18:40:47 -0800

    style the area page. will need to add some more, but looks good for now.

--- client/src/pages/Areas/Areas.js
+++ client/src/pages/Areas/Areas.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import AreaCard from "../../components/AreaCard/AreaCard";
+import "./Areas.scss";
 
 const areaApi = "http://localhost:8080/api/v1/areas";
 
@@ -20,7 +21,7 @@ export default function Areas() {
 
   return (
     <article className="area-section">
-      <h3 className="area-section__title">Areas</h3>
+      <h2 className="area-section__title">Areas</h2>
       {areas.length !== 0 ? (
         areas.map((area) => <AreaCard key={area.id} area={area} />)
       ) : (
