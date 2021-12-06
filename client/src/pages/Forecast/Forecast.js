7773714fbbed1f4285d88f0c7cf9a870eac0ac42
Joshua Young <prgrssn@gmail.com>
2021-12-05 22:52:35 -0800

    organize api response to produce forecast

--- client/src/pages/Forecast/Forecast.js
+++ client/src/pages/Forecast/Forecast.js
@@ -1,34 +1,52 @@
 import "./Forecast.scss";
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import { SettingsSystemDaydreamRounded } from "@mui/icons-material";
 
-const forecastAPI = process.env.REACT_APP_AV_CAN_API;
-const header = { "x-api-key": process.env.REACT_APP_AV_CAN_KEY };
-
-console.log(forecastAPI);
+const forecastAPI = process.env.REACT_APP_AVCAN_API;
+const header = { "x-api-key": process.env.REACT_APP_AVCAN_KEY };
 
 export default function Forecast() {
-  //   useEffect(() => {
-  //     const config = {
-  //       method: "get",
-  //       url: "https://api.avalanche.ca/forecasts/en/products/e7677e53-9c1f-4a6d-8a38-a53c6e5244cd",
-  //       headers: {
-  //         "x-api-key": "uthziK49IH5MHweAyVl5a1gusaPq4EvmvVCEaf5g",
-  //       },
-  //     };
+  const [area, setArea] = useState();
+  const [summaries, setSummaries] = useState();
+  const [travelAdvice, setTravelAdvice] = useState();
+  const [dateIssued, setDateIssued] = useState();
+  const [validUntil, setValidUntil] = useState();
+  const [highlights, setHighlights] = useState();
 
-  //     axios(config)
-  //       .then(function (response) {
-  //         console.log(response.data);
-  //       })
-  //       .catch(function (error) {
-  //         console.log(error);
-  //       });
-  //   });
+  useEffect(() => {
+    const config = {
+      method: "get",
+      url: "https://api.avalanche.ca/forecasts/en/products/e7677e53-9c1f-4a6d-8a38-a53c6e5244cd",
+      headers: {
+        "x-api-key": "uthziK49IH5MHweAyVl5a1gusaPq4EvmvVCEaf5g",
+      },
+    };
+
+    axios(config)
+      .then(function (response) {
+        const report = response.data;
+        setArea(report.area.name);
+        setDateIssued(report.report.dateIssued);
+        setValidUntil(report.report.validUntill);
+        setHighlights(report.report.highlights);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  console.log(area);
+  console.log(dateIssued);
+  console.log(validUntil);
+  console.log(highlights);
+  console.log("");
 
   return (
     <main>
       <h1>Forecast</h1>
+      <p>Area: {area}</p>
+      <div>{highlights}</div>
     </main>
   );
 }
